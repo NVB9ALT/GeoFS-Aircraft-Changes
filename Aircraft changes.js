b4b012fbe40721d634757fef0578eec7fa7dcdf7
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2022-05-04 09:23:45 -0700

    Update Aircraft changes.js

--- Aircraft changes.js
+++ Aircraft changes.js
@@ -33,6 +33,60 @@ function checkAndImplementChanges() {
       geofs.aircraft.instance.engines[0].thrust = 6250
 		geofs.aircraft.instance.engines[1].thrust = 6250
    }
+   if (geofs.aircraft.instance.id = 2) {
+	   //C172 physics from AriakimTaiyo
+let lastTorque = 0;
+let engtorquemp = 0;
+let elevtorquemp = 0;
+let ailtorquemp = 0;
+let rudtorquemp = 0;
+//basic maths to figure out what the engine torque is, then apply it.
+function tqmaths() {
+  engtorquemp = -(lastTorque - geofs.animation.values.rpm) * 3;
+    geofs.aircraft.instance.rigidBody.applyTorqueImpulse([splitAxes(engtorquemp - elevtorquemp)[0],splitAxes(engtorquemp - ailtorquemp)[1],splitAxes(engtorquemp + rudtorquemp)[2]])
+};
+function getEngineTorque() {
+  lastTorque = geofs.animation.values.rpm
+  setTimeout(tqmaths, 200)
+};
+//propwash stuff
+function getControlWash() {
+  elevtorquemp = (geofs.animation.values.rpm / 10) * geofs.animation.values.pitch;
+  ailtorquemp = (geofs.animation.values.rpm / 10) * geofs.animation.values.roll;
+  rudtorquemp = (geofs.animation.values.rpm / 10) * geofs.animation.values.yaw;
+}
+//more complicated maths to resolve torque axes
+function splitAxes(force) {
+  var absangle = geofs.animation.values.heading360;
+  var anglex = (absangle) * (Math.PI/180);
+  var angley = (absangle - 360) * (Math.PI/180);
+  var anglez = geofs.animation.values.atilt;
+  fx = force * (Math.cos(anglex))
+  fy = force * (Math.cos(angley))
+  fz = force * (Math.sin(anglez * (Math.PI/180)))
+  return [fx, fy, fz];
+}
+function doForces() {
+  getEngineTorque()
+}
+//stall and ground effect stuff
+function stallForces() {
+  if (geofs.animation.values.aoa > 10) {
+    geofs.aircraft.instance.rigidBody.applyTorqueImpulse([splitAxes(Math.random()*geofs.animation.values.aoa * 2)[0],splitAxes(Math.random()*geofs.animation.values.aoa * 2)[1],0])
+  }
+}
+function groundEffect() {
+  if (geofs.animation.values.haglFeet <= 10) {
+    geofs.aircraft.instance.rigidBody.applyCentralImpulse([0,0,(-(geofs.animation.values.haglFeet) + 10) * geofs.animation.values.kias])
+  }
+}
+setInterval(function(){
+  groundEffect();
+  stallForces();
+  getControlWash();
+  doForces();
+}, 200)
+};
    if (geofs.aircraft.instance.id == 1){
 	   //And this turns the Piper Cub into a Super Cub
 		//Much better for the Colorado Rockies
