8fb06ab0df99f6a9aa6eef7cacbf1471cb62ed7d
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2022-10-16 11:48:46 -0700

    Update Aircraft-fixes.js

--- Aircraft-fixes.js
+++ Aircraft-fixes.js
@@ -46,7 +46,7 @@ if (geofs.aircraft.instance.id == 7) {
    };
 };
 //Su-35 gets thrust vectoring on the yaw axis and better physics
-if (geofs.aircraft.instance.id == 18 && geofs.aircraft.instance.liveryId != 4){
+if (geofs.aircraft.instance.id == 18 && geofs.aircraft.instance.liveryId != 4 geofs.aircraft.instance.liveryId != 1){
 geofs.aircraft.instance.definition.parts[3].area = 15
 geofs.aircraft.instance.definition.parts[4].area = 15
 geofs.aircraft.instance.definition.parts[2].area = 17
